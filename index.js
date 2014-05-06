646d62bee55a9f9d4ae173455c1ac35e63ead1b5
Etan Lightstone <etan@newrelic.com>
2014-05-06 12:55:50 -0700

    change port to 80

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/userlist', function(req, res) {
     });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
