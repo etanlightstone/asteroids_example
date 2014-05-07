4264996376b3224ea5b1f3849920faf1561cbf8d
Etan Lightstone <etan@newrelic.com>
2014-05-06 17:16:36 -0700

    add readme, get rid of useless route

---
+++ README.md
@@ -0,0 +1,24 @@
+Requirements: nodeJS and MongoDB
+
+Importing data:
+copy MPCORB.DAT into the node project folder form here: http://www.minorplanetcenter.org/iau/MPCORB/MPCORB.DAT
+
+import by running:
+> node planetsimport.js
+Note: Watch CPU usage of the node process, it doesn't exist it on its own :)  control-c when the CPU usage drops to zero.. its done!
+
+Run using: 
+> node index.js
+
+then visit: 
+http://localhost:3000/
+(warning that UI is half backed)
+
+
+Getsome data:
+
+http://localhost:3000/planets/list/10/12       (skips the first 10, shows 12 in order)
+
+http://localhost:3000/planets/00017  (get a specific astroid by designation)
+
+
--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-//var http = require('http');
 var express = require('express');
 
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('mongodb://heroku_app24929585:oj935gaq2mluefeejeeo930vls@ds029807.mongolab.com:29807/heroku_app24929585'); 
-//monk('localhost:27017/planetsdb');
-
 var app = express();
 var planetsDB = db.get('planetscollection');
 
 
-app.get('/planets/', function(req, res){
-  var peeps = [{name: "bob", size:12}, {name: "Juan", size:12}, {name: "Jason", size:12}, {name: "Zander", size:12}];
-  res.json(peeps);
-});
 
 app.get('/planets/list/:from/:total', function(req, res){
   planetsDB.find({},{skip:parseInt(req.params.from), limit:parseInt(req.params.total)},function(e,docs){
